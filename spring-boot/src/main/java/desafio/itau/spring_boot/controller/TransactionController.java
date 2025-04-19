ce993fbee922af50d47ff4dd393e22524e59e79a
bielbarros <gabrielbarrosg11@gmail.com>
2025-04-19 19:52:41 -0300

    feat: Implement controllers for transaction and statistics endpoints

---
+++ spring-boot/src/main/java/desafio/itau/spring_boot/controller/TransactionController.java
@@ -0,0 +1,43 @@
+package desafio.itau.spring_boot.controller;
+
+import desafio.itau.spring_boot.dto.TransactionRequest;
+import desafio.itau.spring_boot.model.Transaction;
+import desafio.itau.spring_boot.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTransaction(@Valid @RequestBody TransactionRequest request){
+        if (request.getDataHora().isAfter(OffsetDateTime.now()) || request.getValor() <= 0) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearTransactions() {
+        transactionService.clearTransactions();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
+
+
+}
